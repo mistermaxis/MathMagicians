b79fe56bbee9dd9eaa306da62ae56e25efe25bd0
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-25 15:19:11 -0300

    Fix project errors.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
@@ -14,3 +15,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals(App());
+reportWebVitals(React);
