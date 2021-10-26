5f4713e18bb7ab85f90a6e50e8315e18de5e9292
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-26 18:38:10 -0300

    Remove logo files & change App to class component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/calculator.js'; // eslint-disable-line no-unused-vars
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Math Magicians</h1>
-      </header>
-      <Calculator/>
-    </div>
-  );
+class App extends React.Component {
+  render() { // eslint-disable-line class-methods-use-this
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Math Magicians</h1>
+        </header>
+        <Calculator/>
+      </div>
+    );
+  }
 }
 
 export default App;
