4af2cc8eff3fcd71681e08b698309e334f463a15
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-28 13:49:30 -0300

    Moved handlePress below useEffect.

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -6,11 +6,6 @@ const Calculator = () => {
   const [result, setResult] = useState(0);
   const calcData = { total: null, next: null, operation: null };
 
-  useEffect(() => {
-    const buttons = Array.from(document.querySelectorAll('button'));
-    buttons.forEach((button) => button.addEventListener('click', handlePress));
-  }, []);
-
   const handlePress = (e) => {
     const pressedButton = e.currentTarget;
     const buttonName = pressedButton.innerText;
@@ -28,32 +23,37 @@ const Calculator = () => {
     calcData.total = newResult.total;
     calcData.next = newResult.next;
     calcData.operation = newResult.operation;
-  }
+  };
 
-    return (
-      <div className="calculator-container">
-          <span className="result">{result}</span>
-          <button className="grey-button" type="button">AC</button>
-          <button className="grey-button" type="button">+/-</button>
-          <button className="grey-button" type="button">%</button>
-          <button className="orange-button" type="button">÷</button>
-          <button className="grey-button" type="button">7</button>
-          <button className="grey-button" type="button">8</button>
-          <button className="grey-button" type="button">9</button>
-          <button className="orange-button" type="button">x</button>
-          <button className="grey-button" type="button">4</button>
-          <button className="grey-button" type="button">5</button>
-          <button className="grey-button" type="button">6</button>
-          <button className="orange-button" type="button">-</button>
-          <button className="grey-button" type="button">1</button>
-          <button className="grey-button" type="button">2</button>
-          <button className="grey-button" type="button">3</button>
-          <button className="orange-button" type="button">+</button>
-          <button className="n0 grey-button" type="button">0</button>
-          <button className="grey-button" type="button">.</button>
-          <button className="orange-button" type="button">=</button>
-      </div>
-    );
-}
+  useEffect(() => {
+    const buttons = Array.from(document.querySelectorAll('button'));
+    buttons.forEach((button) => button.addEventListener('click', handlePress));
+  }, []);
+
+  return (
+    <div className="calculator-container">
+        <span className="result">{result}</span>
+        <button className="grey-button" type="button">AC</button>
+        <button className="grey-button" type="button">+/-</button>
+        <button className="grey-button" type="button">%</button>
+        <button className="orange-button" type="button">÷</button>
+        <button className="grey-button" type="button">7</button>
+        <button className="grey-button" type="button">8</button>
+        <button className="grey-button" type="button">9</button>
+        <button className="orange-button" type="button">x</button>
+        <button className="grey-button" type="button">4</button>
+        <button className="grey-button" type="button">5</button>
+        <button className="grey-button" type="button">6</button>
+        <button className="orange-button" type="button">-</button>
+        <button className="grey-button" type="button">1</button>
+        <button className="grey-button" type="button">2</button>
+        <button className="grey-button" type="button">3</button>
+        <button className="orange-button" type="button">+</button>
+        <button className="n0 grey-button" type="button">0</button>
+        <button className="grey-button" type="button">.</button>
+        <button className="orange-button" type="button">=</button>
+    </div>
+  );
+};
 
 export default Calculator;
