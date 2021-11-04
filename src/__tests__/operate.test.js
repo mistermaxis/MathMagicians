6f129c220c6ced1f984ff9a562fef65862182af2
ixboy <ismaelixboy@gmail.com>
2021-11-04 23:28:25 +0100

    test substruction operator

--- src/__tests__/operate.test.js
+++ src/__tests__/operate.test.js
@@ -1,13 +1,21 @@
 import operate from '../logic/operate';
 
 describe('Operate tests', () => {
-    test('Operate adds two integers', () => {
-        const numberOne = 30;
-        const numberTwo = 73;
+  it('Operate adds two integers', () => {
+    const numberOne = 30;
+    const numberTwo = 73;
 
-        const result = operate(numberOne, numberTwo, '+');
+    const result = operate(numberOne, numberTwo, '+');
 
-        expect(result).toBe('103');
-    });
-    
-})
+    expect(result).toBe('103');
+  });
+
+  it('substructs from an integer', () => {
+    const numberOne = 225;
+    const numberTwo = 100;
+
+    const result = operate(numberOne, numberTwo, '-');
+
+    expect(result).toBe('125');
+  });
+});
